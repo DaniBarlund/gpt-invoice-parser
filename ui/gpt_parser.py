46159630a9de76cf1870dee81d6735e2d9a8e3ac
Dani Bärlund <danibarlund@gmail.com>
2024-07-30 10:14:14 +0300

    added feature to return list of columns in the invoice

--- ui/gpt_parser.py
+++ ui/gpt_parser.py
@@ -3,12 +3,15 @@
 #       streaming answer generation.
 
 from openai import OpenAI
+import instructor
+from pydantic import BaseModel
+from typing import List
 from langchain_community.document_loaders import PyMuPDFLoader
 
-client = OpenAI()
 prompt = "Extract only the invoice items and summary data from the pdf file into a table."
 
 def get_answer(content:str) -> str:
+    client = OpenAI()
     stream = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
@@ -23,3 +26,40 @@ def read_pdf(invoice_name: str) -> str:
     pdf = loader.load()
     content = " ".join([page.page_content for page in pdf])
     return content
+
+#------------------- INSTRUCTOR -------------------#
+
+
+class Columns(BaseModel):
+    columns : List[str]
+
+def get_columns(content:str) -> list[str]:
+    client = instructor.from_openai(OpenAI())
+    prompt = "Extract only columns of the invoice"
+    columns = client.chat.completions.create(
+    model="gpt-4o-mini",
+    response_model=Columns,
+    messages=[
+        {"role": "user", "content": prompt},
+        {"role": "assistant", "content": content}]
+    )
+    return columns
+
+def create_prompt(columns:list[str])->str:
+    prompt = "Extract only the invoice items listed into a table. Columns are: "
+    for column in columns:
+        prompt = f"{prompt} {column},"
+    return prompt
+
+def get_table(content:str) -> str:
+    client = OpenAI()
+    columns = get_columns(content)
+    prompt = create_prompt(columns)
+    table = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "user", "content": prompt},
+        {"role": "assistant", "content": content}],
+    stream=True
+    )
+    return table
